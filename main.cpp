cd4a498cac07be043fc70b3cee1153b85ef3f4fb
smntin <smn.pankv@gmail.com>
2020-07-26 18:12:47 +0500

    First try with SDL2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include<SDL.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+SDL_Window *g_pWindow = 0;
+SDL_Renderer *g_pRenderer = 0;
+
+int main(int argc, char *args[]) {
+    // initialize SDL
+    if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
+        // if succeeded create our window
+        g_pWindow = SDL_CreateWindow("Chapter 1: Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                     640, 480,
+                                     SDL_WINDOW_SHOWN);
+// if the window creation succeeded create our renderer if(g_pWindow != 0)
+        {
+            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+        }
+    } else {
+        return 1; // sdl could not initialize
+    }
+// everything succeeded lets draw the window
+// set to black // This function expects Red, Green, Blue and // Alpha as color values
+    SDL_SetRenderDrawColor(g_pRenderer,
+                           0, 0, 0, 255);
+    while (true) {
+// set to black
+        SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+        // clear the window to black
+        SDL_RenderClear(g_pRenderer);
+        // show the window
+        SDL_RenderPresent(g_pRenderer);
+    }
+// clean up SDL
+    SDL_Quit();
+
     return 0;
 }
